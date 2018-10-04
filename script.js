fde92f720ecec15c72b6b48b8a2c6d13dbd44504
Regina <regina-saifieva@mail.ru>
2018-10-04 21:37:51 +0300

    second

--- script.js
+++ script.js
@@ -3,11 +3,8 @@
 var num = 33721;
  
 var newAnswer = 3 * 3 * 7 * 2; 
-
-console.log(newAnswer);
-
 var extent = newAnswer;
 var s = extent ** 2 * extent;
+console.log(newAnswer);
 console.log(s);
-alert(newAnswer);
-alert(s);
+alert(Math.floor(s/100000));
