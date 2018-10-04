d66065aa98d6aa2c5f9a865bc2d2e7a43fbfeebc
Regina <regina-saifieva@mail.ru>
2018-10-04 18:06:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict'; 
+
+var num = 33721;
+ 
+var newAnswer = 3 * 3 * 7 * 2; 
+
+console.log(newAnswer);
+
+var extent = newAnswer;
+var s = extent ** 2 * extent;
+console.log(s);
+alert(newAnswer);
+alert(s);
